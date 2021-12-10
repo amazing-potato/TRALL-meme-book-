efbaff8feb0a373fda8296727cd018fb03de4941
amazing-potato <81885792+amazing-potato@users.noreply.github.com>
2021-12-10 18:52:40 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = ["troll face.png","doge.jpg","thomas the tank engine.png","AMONG US!!!.png","what.png","idk.png"]
+var names = ["troll face","doge", "thomas the dank engine", "sussy imposter", "what", "idk"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
